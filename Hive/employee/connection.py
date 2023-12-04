ac2411505b469cc6d90798dd866fe1505fe586bd
yassinessadi <yasine.esadi@gmail.com>
2023-12-04 21:44:55 +0100

    add and build airflow image

--- Hive/employee/connection.py
+++ Hive/employee/connection.py
@@ -1,12 +1,13 @@
 from pyhive import hive 
 import requests as req
+import pandas as pd
 
 ###################################################
 #    create tables and insert into customers     #
 ##################################################
 def customersTable(cursor):
     table_creation_query = """
-        CREATE TABLE IF NOT EXISTS testdb.producers_gogo (
+        CREATE TABLE IF NOT EXISTS testdb.customers (
             account_history STRING,
             avg_transaction_value DOUBLE,
             customer_id STRING,
@@ -35,6 +36,20 @@ def transactionsTable(cursor):
     cursor.execute(table_creation_query)
 
 
+
+###################################################
+#   create tables and insert into blacklist_info  #
+##################################################
+def blacklistInfoTable(cursor):
+    table_creation_query = """
+        CREATE TABLE IF NOT EXISTS testdb.blacklist_info(
+            name STRING
+        )
+        """
+    cursor.execute(table_creation_query)
+
+
+
 ###################
 # connect to hive #
 ##################
@@ -61,13 +76,14 @@ def get_all_data(cursor):
     """
     get all the customers
     """
-    cursor.execute(f'SELECT * FROM testdb.transactions')
+    cursor.execute(f'SELECT * FROM testdb.blacklist_info')
     return cursor.fetchall()
 
 def inseting_query(cursor,query):
     cursor.execute(query)
-
-# insert into customers
+#########################
+# insert into customers #
+#########################
 def insert_into_customers(url_base):
     customers = req.get(url_base)
     if customers.status_code == 200:
@@ -78,7 +94,7 @@ def insert_into_customers(url_base):
             account_history_string = ",".join(data["account_history"])
             # Insert data into the table
             insert_query = '''
-                INSERT INTO testdb.producers_gogo
+                INSERT INTO testdb.customers
                 VALUES ('{a}',
                 '{b}',
                 '{c}',
@@ -115,21 +131,90 @@ def insert_into_customers(url_base):
     else:
         print("Failed to fetch data from the API.")
 
-def insert_into_external_data(url_base):
+###############################
+# insert into black list info #
+###############################
+def insert_blacklist_info(url_base):
     customers = req.get(url_base)
     if customers.status_code == 200:
-        result = customers.json()
         counter = 0
+        data = customers.json()['blacklist_info']
         while True:
-            data = customers.json()[counter]
-            data['']
+            insert_query = '''
+            INSERT INTO testdb.blacklist_info
+            VALUES ('{a}')
+            '''.format(
+                a = data[counter]
+            )
+            connection,cursor = connectHive()
+
+            # create customer tables
+            blacklistInfoTable(cursor)
+
+
+            # insert into customers
+            # inseting_query(cursor=cursor,query=insert_query)
+
+            # save changes
+            savesChanges(connection)
+            print(data[counter])
+            # get all data
+            # print(get_all_data(cursor=cursor))
+            # close connection
+            closeConnection(cursor=cursor,connection=connection)
             counter += 1
-            if counter == len(result) :
+            if counter == len(data) :
                 break
+
+
+
+###############################
+# insert into black list info #
+###############################
+def insert_fraud_details(url_base):
+    customers = req.get(url_base)
+    if customers.status_code == 200:
+        counter = 0
+        fraud_reports = customers.json()['fraud_reports']
+        credit_scores = customers.json()['credit_scores']
+        df_fraud_reports = pd.DataFrame(fraud_reports,columns=[['user_id','fraud_reports']])
+        df_credit_scores = pd.DataFrame(credit_scores,columns=[['user_id','credit_scores']])
+        # df = pd.merge(df_credit_scores,df_fraud_reports,on=['user_id'],how='inner')
+        print(credit_scores)
+        # while True:
+        #     insert_query = '''
+        #     INSERT INTO testdb.blacklist_info
+        #     VALUES ('{a}')
+        #     '''.format(
+        #         a = data[counter]
+        #     )
+        #     connection,cursor = connectHive()
+
+        #     # create customer tables
+        #     blacklistInfoTable(cursor)
+
+
+        #     # insert into customers
+        #     # inseting_query(cursor=cursor,query=insert_query)
+
+        #     # save changes
+        #     savesChanges(connection)
+        #     print(data[counter])
+        #     # get all data
+        #     # print(get_all_data(cursor=cursor))
+        #     # close connection
+        #     closeConnection(cursor=cursor,connection=connection)
+        #     counter += 1
+        #     if counter == len(data) :
+        #         break
 
 
 # DROP TABLE IF EXISTS testdb.users PURGE;
 
+
+########################################
+# insert into insert_into_transactions #
+########################################
 def insert_into_transactions(url_base):
     customers = req.get(url_base)
     if customers.status_code == 200:
@@ -190,16 +275,21 @@ def insert_into_transactions(url_base):
 
 
 #############################
+# insert into black list #
+#############################
+
+# insert_blacklist_info("http://127.0.0.1:5000/api/external_data/")
+
+
+#############################
 # insert into external data #
 #############################
 
-# insert_into_external_data("http://127.0.0.1:5000/api/external_data/")
-
-
+insert_fraud_details("http://127.0.0.1:5000/api/external_data/")
 
 #############################
 # insert into transactions  #
 #############################
 
 
-insert_into_transactions("http://127.0.0.1:5000/api/transactions/")
+# insert_into_transactions("http://127.0.0.1:5000/api/transactions/")
